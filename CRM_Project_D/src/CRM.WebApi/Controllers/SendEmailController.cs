7ae4e2269ed0858d44a0bb5eb6c6e73bd22069d1
merisahakyan <sahakyan_m@bk.ru>
2017-05-23 21:50:06 +0400

    sending emails with SendGrid

--- CRM_Project_D/src/CRM.WebApi/Controllers/SendEmailController.cs
+++ CRM_Project_D/src/CRM.WebApi/Controllers/SendEmailController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,6 +11,27 @@ namespace CRM.WebApi.Controllers
 {
     public class SendEmailController : ApiController
     {
+        public IHttpActionResult PostSendTo([FromUri] string address)
+        {
+            SendMail(address);
+            return Ok("sended");
+        }
+        private void SendMail(string sendto)
+        {
+            try
+            {
+                var client = new SendGridClient("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");
+                var myMessage = new SendGridMessage();
+                myMessage.AddTo(sendto);
+                myMessage.From = new EmailAddress("mssend@ms.com");
+                myMessage.Subject = "subject";
+                myMessage.PlainTextContent = "body";
+                var response = client.SendEmailAsync(myMessage);
+            }
+            catch
+            {
 
+            }
+        }
     }
 }
