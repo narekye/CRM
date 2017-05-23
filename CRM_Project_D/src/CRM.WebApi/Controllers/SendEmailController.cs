2000490fb75b7a1636a02dd05cd9a4814ec13558
merisahakyan <sahakyan_m@bk.ru>
2017-05-23 22:15:07 +0400

    another way to send mails

--- CRM_Project_D/src/CRM.WebApi/Controllers/SendEmailController.cs
+++ CRM_Project_D/src/CRM.WebApi/Controllers/SendEmailController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Mail;
+using System.Net.Mime;
 using System.Web.Http;
 
 namespace CRM.WebApi.Controllers
@@ -14,24 +16,43 @@ namespace CRM.WebApi.Controllers
         public IHttpActionResult PostSendTo([FromUri] string address)
         {
             SendMail(address);
-            return Ok("sended");
+            return Ok();
+        }
+        public IHttpActionResult PostSendToList(List<string> addresses)
+        {
+            foreach (var m in addresses)
+                SendMail(m);
+            return Ok();
         }
         private void SendMail(string sendto)
         {
             try
             {
-                var client = new SendGridClient("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");
-                var myMessage = new SendGridMessage();
-                myMessage.AddTo(sendto);
-                myMessage.From = new EmailAddress("mssend@ms.com");
-                myMessage.Subject = "subject";
-                myMessage.PlainTextContent = "body";
-                var response = client.SendEmailAsync(myMessage);
+                MailMessage mailMsg = new MailMessage();
+
+                // To
+                mailMsg.To.Add(new MailAddress(sendto));
+
+                // From
+                mailMsg.From = new MailAddress("forproj@ms.com");
+
+                // Subject and multipart/alternative Body
+                mailMsg.Subject = "subject";
+                string text = "text body";
+                mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(text, null, MediaTypeNames.Text.Plain));
+
+                // Init SmtpClient and send
+                SmtpClient smtpClient = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(587));
+                System.Net.NetworkCredential credentials = new System.Net.NetworkCredential("sahakyan_meri", "p42Zmx39");
+                smtpClient.Credentials = credentials;
+
+                smtpClient.Send(mailMsg);
             }
             catch
             {
 
             }
+
         }
     }
 }
