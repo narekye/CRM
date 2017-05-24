d42fadee39a08eb190b2de207334450840dda310
narekye <yegoryan.narek@gmail.com>
2017-05-24 00:55:39 -0600

    SendEmail Controller changes.

--- CRM_Project_D/src/CRM.WebApi/Controllers/SendEmailController.cs
+++ CRM_Project_D/src/CRM.WebApi/Controllers/SendEmailController.cs
@@ -1,10 +1,5 @@
-﻿using SendGrid;
-using SendGrid.Helpers.Mail;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Net.Mail;
 using System.Net.Mime;
 using System.Web.Http;
