5f862f1140ac3e4d57fb3b62d822df7297686530
merisahakyan <sahakyan_m@bk.ru>
2017-05-27 21:00:51 +0400

    crypting username and password

--- CRM_Project_D/src/CRM.WebApi/InfrastructureModel/MailManager.cs
+++ CRM_Project_D/src/CRM.WebApi/InfrastructureModel/MailManager.cs
@@ -9,8 +9,31 @@
     using System.Net;
     using System.Net.Configuration;
     using Entities;
+    using System.Security.Cryptography;
+    using System.IO;
+    using System.Text;
+
     public class MailManager : IDisposable
     {
+        static readonly string PasswordHash = "P@@Sw0rd";
+        static readonly string SaltKey = "S@LT&KEY";
+        static readonly string VIKey = "@1B2c3D4e5F6g7H8";
+        string Decrypt(string encryptedText)
+        {
+            byte[] cipherTextBytes = Convert.FromBase64String(encryptedText);
+            byte[] keyBytes = new Rfc2898DeriveBytes(PasswordHash, Encoding.ASCII.GetBytes(SaltKey)).GetBytes(256 / 8);
+            var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.None };
+
+            var decryptor = symmetricKey.CreateDecryptor(keyBytes, Encoding.ASCII.GetBytes(VIKey));
+            var memoryStream = new MemoryStream(cipherTextBytes);
+            var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+            byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+
+            int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+            memoryStream.Close();
+            cryptoStream.Close();
+            return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount).TrimEnd("\0".ToCharArray());
+        }
         private readonly CRMContext _database = new CRMContext();
 
         public bool SendMail(string sendto, int templateid)
@@ -32,8 +55,8 @@
                 int port = mailSettings.Smtp.Network.Port;
                 string from = mailSettings.Smtp.From;
                 string host = mailSettings.Smtp.Network.Host;
-                string pwd = mailSettings.Smtp.Network.Password;
-                string uid = mailSettings.Smtp.Network.UserName;
+                string pwd = Decrypt(mailSettings.Smtp.Network.Password);
+                string uid = Decrypt(mailSettings.Smtp.Network.UserName);
 
                 var message = new MailMessage
                 {
