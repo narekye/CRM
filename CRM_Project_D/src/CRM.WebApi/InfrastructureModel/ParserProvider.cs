66c10801138c19abbf652fc616e9c88a5d4de96c
merisahakyan <sahakyan_m@bk.ru>
2017-06-01 23:23:28 +0400

    CSV upload with checking columns

--- CRM_Project_D/src/CRM.WebApi/InfrastructureModel/ParserProvider.cs
+++ CRM_Project_D/src/CRM.WebApi/InfrastructureModel/ParserProvider.cs
@@ -165,54 +165,91 @@
             string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "file.csv";
             try
             {
+                int index = -1, index1 = -1, index2 = -1;
                 File.WriteAllBytes(path, bytes);
                 string[] lines = File.ReadAllLines(path);
                 string[] allcolumns = lines[0].Split(',');
-                Dictionary<string, int> d = new Dictionary<string, int>();
-                d.Add("FullName", 0);
-                d.Add("Company", 1);
-                d.Add("Position", 2);
-                d.Add("Country", 3);
-                d.Add("Email", 4);
-                d.Add("DataInserted", 5);
+
                 if (!Checking(allcolumns.ToList(), ref columns))
                     return null;
-                    for (int i = 1; i < lines.Length; i++)
+                Dictionary<string, int> d = new Dictionary<string, int>();
+                if (Exist(allcolumns.ToList(), "fullname", out index)
+                || Exist(allcolumns.ToList(), "full name", out index1))
+                {
+                    if (index1 == -1)
+                        d.Add("FullName", index);
+                    else
+                        d.Add("FullName", index1);
+                }
+                if (Exist(allcolumns.ToList(), "company", out index)
+                    || Exist(allcolumns.ToList(), "companyname", out index1)
+                    || Exist(allcolumns.ToList(), "company name", out index2))
+                {
+                    if (index1 == -1 && index2 == -1)
+                        d.Add("Company", index);
+                    else if (index1 == -1)
+                        d.Add("Company", index2);
+                    else
+                        d.Add("Company", index1);
+                }
+                if (Exist(allcolumns.ToList(), "position", out index))
+                    d.Add("Position", index);
+                if (Exist(allcolumns.ToList(), "country", out index))
+                    d.Add("Country", index);
+                if (Exist(allcolumns.ToList(), "email", out index)
+                    || Exist(allcolumns.ToList(), "mail", out index1))
+                {
+                    if (index1 == -1)
+                        d.Add("Email", index);
+                    else
+                        d.Add("Email", index1);
+                }
+                if (Exist(allcolumns.ToList(), "data inserted", out index)
+                    || Exist(allcolumns.ToList(), "datainserted", out index1))
+                {
+                    if (index1 == -1)
+                        d.Add("DataInserted", index);
+                    else
+                        d.Add("DataInserted", index1);
+                }
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    Contact contact = new Contact();
+                    string[] values = lines[i].Split(',');
+                    for (int j = 0; j < values.Length; j++)
                     {
-                        Contact contact = new Contact();
-                        string[] values = lines[i].Split(',');
-                        for (int j = 0; j < values.Length; j++)
+                        switch (j)
                         {
-                            switch (j)
-                            {
-                                case 0:
-                                    contact.FullName = values[d["FullName"]];
-                                    break;
-                                case 1:
-                                    contact.CompanyName = values[d["Company"]];
-                                    break;
-                                case 2:
-                                    contact.Position = values[d["Position"]];
-                                    break;
-                                case 3:
-                                    contact.Country = values[d["Country"]];
-                                    break;
-                                case 4:
-                                    contact.Email = values[d["Email"]];
-                                    break;
-                                case 5:
-                                    contact.DateInserted = Convert.ToDateTime(values[d["DataInserted"]]);
-                                    break;
-                            }
+                            case 0:
+                                contact.FullName = values[d["FullName"]];
+                                break;
+                            case 1:
+                                contact.CompanyName = values[d["Company"]];
+                                break;
+                            case 2:
+                                contact.Position = values[d["Position"]];
+                                break;
+                            case 3:
+                                contact.Country = values[d["Country"]];
+                                break;
+                            case 4:
+                                contact.Email = values[d["Email"]];
+                                break;
+                            case 5:
+                                contact.DateInserted = Convert.ToDateTime(values[d["DataInserted"]]);
+                                break;
                         }
-                        contact.GuID = new Guid();
-                        contactslist.Add(contact);
                     }
+                    contact.GuID = new Guid();
+                    contactslist.Add(contact);
+                }
                 File.Delete(path);
             }
-            catch
+            catch (Exception ex)
             {
                 File.Delete(path);
+                Console.WriteLine(ex.Message);
             }
             return contactslist;
         }
